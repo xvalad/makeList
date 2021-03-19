71e145db72b638e97e10da7b3fb66047af38b3cc
xvalad <adams.vallejos@gu.se>
2021-03-19 17:59:46 +0100

    Updated: fre 19 mar 2021 17:59:46 CET

--- tmp-compress.sh
+++ tmp-compress.sh
@@ -5,5 +5,7 @@
 path='/volume_b/volume_c' # Without the final '/'
 for i in $(less tmp-compress.lst)
 do
+	echo "Compressing: ${i}"
 	tar cvfj $i.tar.bz2 $path/$i/ > $i.tar.bz2.log 2> $i.tar.bz2.err
+	echo "Job finished, files generated: `ls ${i}.*`"
 done
