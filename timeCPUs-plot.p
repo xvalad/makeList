9a95b07d3136c9d30b6814238e9d1a4efbebf809
xvalad <adams.vallejos@gu.se>
2021-03-29 11:05:45 +0200

    Updated: mån 29 mar 2021 11:05:45 CEST

--- timeCPUs-plot.p
+++ timeCPUs-plot.p
@@ -1,10 +1,11 @@
+#gnuplot -e "filename='foo.tsv'" timeCPUs-plot.p
 # pleamble
 set terminal png size 500,500
-set output 'timeCPUs-plot.png'
+set output filename . '.png'
 set title 'time elapsed vs CPUs'
 set style line 5 lt rgb "blue" lw 2 pt 6
 #set logscale y
 
-# Read file: timeVsCPU.tsv
+# Read .tsv file
 set datafile separator "\t"
-plot "timeVsCPU.tsv" using 1:2 with linespoints ls 5
+plot filename using 1:2 with linespoints ls 5
--- timeCPUs.sh
+++ timeCPUs.sh
@@ -5,9 +5,9 @@ file="timeCPU_$HOSTNAME.tsv"
 rm -f $file
 for i in `seq $NPROC`
 do
-	start=$(date +%s)
-	indexamajig -i files.lst -g 5HT2B-Liu-2013-predrefine.geom --peaks=hdf5 --int-radius=3,4,5 -o tutorial.stream --indexing=mosflm -p 5HT2B.cell -j ${i}
-	end=$(date +%s)
-	elapsed=$((end-start))
-	echo -e "$i\t$elapsed" >> $file
+        start=$(date +%s)
+        indexamajig -i files.lst -g 5HT2B-Liu-2013-predrefine.geom --peaks=hdf5 --int-radius=3,4,5 -o tutorial_$HOSTNAME.stream --indexing=mosflm -p 5HT2B.cell -j ${i}
+        end=$(date +%s)
+        elapsed=$((end-start))
+        echo -e "$i\t$elapsed" >> $file
 done
