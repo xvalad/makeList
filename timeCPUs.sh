ea0229f5dd137c4271d867c9027feb004879cffc
xvalad <adams.vallejos@gu.se>
2021-04-08 01:15:12 +0200

    Updated: tor  8 apr 2021 01:15:12 CEST

--- timeCPUs.sh
+++ timeCPUs.sh
@@ -3,7 +3,7 @@ HOSTNAME=`hostname`
 NPROC=$(grep proc /proc/cpuinfo | wc -l )
 file="timeCPU_$HOSTNAME.dat"
 rm -f $file
-for i in `seq $NPROC`
+for i in $(eval echo {$NPROC..1..-3}) # `seq $NPROC`
 do
         start=$(date +%s)
         indexamajig -i files.lst -g 5HT2B-Liu-2013-predrefine.geom --peaks=hdf5 --int-radius=3,4,5 -o tutorial_$HOSTNAME.stream --indexing=mosflm -p 5HT2B.cell -j ${i}
