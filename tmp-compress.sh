aa89f8b3f88c0cf391c2caf680402b4b5a2d04fb
xvalad <adams.vallejos@gu.se>
2021-04-15 11:28:31 +0200

    Updated: tor 15 apr 2021 11:28:31 CEST

--- tmp-compress.sh
+++ tmp-compress.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# Script to compres a list of folders for bacup on tape
+# Script to compres a list of folders for backup on tape
 #
-path='/volume_b/volume_c' # Without the final '/'
-for i in $(less tmp-compress.lst)
+path='/volume_b/volume_c/stanford2' # Without the final '/'
+for i in $(cat tmp-compressionList.lst)
 do
 	echo "Compressing: ${i}"
-	tar cvfj $i.tar.bz2 $path/$i/ > $i.tar.bz2.log 2> $i.tar.bz2.err
-	echo "Job finished, files generated: `ls ${i}.*`"
+	tar cvfj $path/$i.tar.bz2 $path/$i/ > $path/$i.tar.bz2.log 2> $path/$i.tar.bz2.err
+	echo "Job finished, files generated: `ls ${path}/${i}.*`"
 done
