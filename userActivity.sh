44c98c227c70eadafbc12603624f3f3ada8a726f
xvalad <adams.vallejos@gu.se>
2021-04-28 11:39:49 +0200

    Updated: ons 28 apr 2021 11:39:49 CEST

--- userActivity.sh
+++ userActivity.sh
@@ -5,17 +5,14 @@
 SERVER=$(hostname)
 CPUS=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
 DATE=$(date +"%Y/%m/%d-%H:%M")
-VARIABLE=$(top -b -n 1 -u "$USERNAME" | awk 'NR>7 { sum += $9/100; } END { print sum; }')
+VARIABLE=$(printf "%.0f\n" `top -b -n 1 -u "$USERNAME" | awk 'NR>7 { sum += $9/100; } END { print sum; }'`)
 for USER in $(who | awk '{print $1}' | sort -u)
 do
 	if [ "$USER" = "bruno" ]; then continue; fi
 	if [ "$USER" = "ladm" ]; then continue; fi
- 	if [ $(VARIABLE) -lt 6 ]; then 
- 		# echo `date "+%y/%m/%d %H:%M:%S"`" $USER "$VARIABLE # >> /home/adams/backupStatus/cpuActivity/userActivity/$SERVER-usage-$USER.csv
- 		echo "case a"
- 	elif [ $VARIABLE -lt 6 ]; then
- 		echo "case b"
+ 	if [ $VARIABLE -gt 6 ]; then 
+ 		echo `date "+%y/%m/%d %H:%M:%S"`" $USER $VARIABLE" # >> /home/adams/backupStatus/cpuActivity/userActivity/$SERVER-usage-$USER.csv
 	else
-		echo "none"
+		continue
 	fi
 done
