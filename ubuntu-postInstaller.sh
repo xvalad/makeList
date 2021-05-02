88017792ce9d149a8f6e3988ae58ed160f334da3
xvalad <adams.vallejos@gu.se>
2021-05-02 21:20:08 +0200

    Updated: sön  2 maj 2021 21:20:08 CEST

---
+++ ubuntu-postInstaller.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#   UBUNTU 21.04 laptop
+#   USAGE: 
+#   sudo chmod +x ubuntu-postInstaller.sh
+#   sudo bash ubuntu-postInstaller.sh
+
+# Update
+sudo apt update; sudo apt upgrade -your
+
+# Add PPAs
+sudo add-apt-repository ppa:papirus/papirus
+
+# Automatically installling programs from Apt
+sudo apt install program1 program2 program3 -your
+
+# Automatically install programs from snap
+sudo snap install snappackage1 snappackage2 snappackage3
+exit
+
+# anaconda
+# git
+# vscode
+# brave
+# teamviewer
+# anydesk     
+# zoom
+# dropbox
+# r 
+# julia
+# matlab - run online
+# vscode
+# nvidia drivers
+# fortran
+# c
+# cpp
